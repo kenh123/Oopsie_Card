b9da5dbd9be7c7bc96eca8370728ecf5e1dd18e3
Kenneth Heiserman <kennethheiserman@yahoo.com>
2023-05-20 00:21:42 -0500

    created PunchedHoles class and its DAO

---
+++ app/src/main/java/com/example/oopsiecard/PunchedHoles.kt
@@ -0,0 +1,8 @@
+package com.example.oopsiecard
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "PunchedHoles")
+class PunchedHoles(
+    @field:PrimaryKey(autoGenerate = true) var holeId: Int, var resourceId: Int)
---
+++ app/src/main/java/com/example/oopsiecard/PunchedHolesDAO.kt
@@ -0,0 +1,15 @@
+package com.example.oopsiecard
+
+import androidx.room.Dao
+import androidx.room.PrimaryKey
+
+@Dao
+interface PunchedHolesDAO {
+
+
+
+
+
+
+}
+
